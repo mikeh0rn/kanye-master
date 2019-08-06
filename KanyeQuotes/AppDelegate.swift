b72daded4ff125ae4b6cea94d32bbc05f5c7abf4
Mike Horn <mikelaurencehorn@gmail.com>
2019-08-06 08:37:22 -0400

    add openURL and contiinueUserActivity

--- KanyeQuotes/AppDelegate.swift
+++ KanyeQuotes/AppDelegate.swift
@@ -34,7 +34,13 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         return true
     }
     
-    //TODO: openURL and continueUserActivity
+    func application(_ app: UIApplication, open url: URL, options: [UIApplication.OpenURLOptionsKey : Any] = [:]) -> Bool {
+        return true
+    }
+    
+    func application(_ application: UIApplication, continue userActivity: NSUserActivity, restorationHandler: @escaping ([UIUserActivityRestoring]?) -> Void) -> Bool {
+        return true
+    }
 
     func applicationWillResignActive(_ application: UIApplication) {
         // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
